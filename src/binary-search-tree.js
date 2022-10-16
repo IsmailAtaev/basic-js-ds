b8f97221acb208c1d65df175b71f4d39a0f6a376
IsmailAtaev <ataewisma@gmail.com>
2022-10-16 23:44:40 +0300

    fix: '\n' and tab BST.file

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -127,17 +127,14 @@ class BinarySearchTree {
     function comparator(node, elem) {
       return node.data === elem ? true : false;
     }
-
     if (data > this.my_root.data) {
       rightTraverse(this.my_root, data, comparator);
     } else {
       leftTraverse(this.my_root, data, comparator);
     }
-
     if (arr.length > 0) {
       return null;
     }
-
     function leftTraverse(node, value, comparator) {
       if (node) {
         leftTraverse(node.left, value, comparator);
@@ -148,7 +145,6 @@ class BinarySearchTree {
         }
       }
     }
-
     function rightTraverse(node, value, comparator) {
       if (node) {
         rightTraverse(node.right, value, comparator);
@@ -160,159 +156,8 @@ class BinarySearchTree {
       }
     }
   }
-
-  /*constructor() {
-    this.myRoot = null;
-  }
-
-  root() {
-    if (!this.myRoot) {
-      return null;
-    } else {
-      return this.myRoot;
-    }
-  }
-
-  add(data) {
-    this.myRoot = addBT(this.myRoot, data);
-
-    function addBT(node, value) {
-      if (!node) {
-        return new Node(value);
-      }
-
-      if (node.value === value) {
-        return node;
-      }
-
-      if (value < node.value) {
-        node.left = addBT(node.left, value);
-      } else {
-        node.right = addBT(node.right, value);
-      }
-      return node;
-      /*
-      return value < node.value
-        ? (node.left = addBT(node.left, value))
-        : (node.right = addBT(node.right, value));
-    
-    }
-  }
-
-  has(data) {
-    return treeHas(this.myRoot, data);
-
-    function treeHas(node, value) {
-
-      if (value === node.value) {
-        return true;
-      }
-      return value < node.value
-        ? treeHas(node.left, value)
-        : treeHas(node.right, value);
-    }
-  }
-
-  find(data) {
-    return findNode(this.myRoot, data);
-
-    function findNode(node, value) {
-      if (!node) {
-        return null;
-      }
-
-      if (node.value === value) {
-        return value;
-      }
-
-      if (value < node.value) {
-        node.left = findNode(node.left, value);
-      } else {
-        node.right = findNode(node.right, value);
-      }
-      return null;
-    }
-  }
-
-  remove(data) {
-    return removeNode(this.myRoot, data);
-
-    function removeNode(node, value) {
-      if (!node) {
-        return null;
-      }
-
-      if (value < node.value) {
-        node.left = removeNode(node.left, value);
-        return node;
-      } else if (node.value < value) {
-        node.right = removeNode(node.right, value);
-        return node;
-      } else {
-        if (!node.left && !node.right) {
-          return null;
-        }
-
-        if (!node.left) {
-          node = node.right;
-          return node;
-        }
-
-        if (!node.right) {
-          node = node.left;
-          return node;
-        }
-
-        let minRight = node.right;
-        while (minRight.left) {
-          minRight = minRight.left;
-        }
-        node.value = minRight.value;
-        node.right = removeNode(node.right, minRight.value);
-        return node;
-      }
-    }
-  }
-
-  /*let node = this.myRoot;
-    if (node.left === null) return node.value;
-    else return this.findMinNode(node.left);
-  min() {
-    if (!this.myRoot) {
-      return;
-    }
-    let node = this.myRoot;
-    while (node.left) {
-      node = node.left;
-    }
-    return node.value;
-  }
-
-  max() {
-    let node = this.myRoot;
-    if (node.right === null) return node.value;
-    else return this.findMinNode(node.right);
-
-    /*if (!this.myRoot) {
-      return;
-    }
-    let node = this.myRoot;
-    while (node.right) {
-      node = node.right;
-    }
-    return node.value;
-  }*/
 }
-
 
 module.exports = {
   BinarySearchTree,
 };
-
-/*
-root 
-    //return this.root != null ? this.root.data : null;
-    //throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-    
-*/
